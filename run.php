46d587999fea8d46c4a6f52633bf8b1ed7081c16
i.takeshi2222 <i.takeshi2222@gmail.com>
2019-10-06 21:52:05 +0900

    通知実装

--- run.php
+++ run.php
@@ -2,6 +2,7 @@
 
 require_once(__DIR__ . "/getPorts.php");
 require_once(__DIR__ . "/reserveBike.php");
+require_once(__DIR__ . "/sendLineMessage.php");
 
 $ports = [
     '00010302' => 'ヨドバシカメラ前',
@@ -9,15 +10,15 @@ $ports = [
     '00010032' => 'UDX駐輪場前',
     '00010037' => '富士ソフト',
     '00010016' => '秋葉原公園',
-    //'00010468' => '高架下ドコモ',
-    //'00010467' => '高架下マーチエキュート',
 ];
 
+$msg="";
 $status = (new GetPorts)->status($ports);
 
-
 foreach ($status as $item) {
-    echo "\n[{$item['stockNum']}]{$item['portName']}";
+    $msg .= "\n[{$item['stockNum']}]{$item['portName']}";
 }
-echo "\n";
-print_r((new ReserveBike)->reserveNearbyBike($status));
+$msg .= "\n";
+$msg .= print_r((new ReserveBike)->reserveNearbyBike($status), true);
+
+new sendLineMessage($msg);
