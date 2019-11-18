3c1cce1de12e3405675283dd64b1c3ec082740b5
iitenkida7 <i.takeshi2222@gmail.com>
2019-11-19 01:56:58 +0900

    構成変更

--- migration/migrate.php
+++ migration/migrate.php
@@ -1,8 +1,16 @@
 <?php
 
-require_once 'bootstrap.php';
+require_once(__DIR__  .'/../libs/bootstrap.php');
 
 use Illuminate\Database\Capsule\Manager as Capsule;
+
+Capsule::schema()->create('users', function ($table) {
+    $table->increments('id');
+    $table->string('line_id');
+    $table->string('chiyokuru_id');
+    $table->integer('chiyokuru_pass');
+    $table->timestamps();
+});
 
 Capsule::schema()->create('report', function ($table) {
     $table->increments('id');
--- port_analyze/checkPortStatu.php
+++ port_analyze/checkPortStatu.php
@@ -1,6 +1,6 @@
 <?php
 require_once( __DIR__ . '/../vendor/autoload.php');
-require_once( __DIR__ . '/bootstrap.php');
+require_once( __DIR__ . '/../libs/bootstrap.php');
 require_once( __DIR__ . '/../getPorts.php');
 
 use Carbon\Carbon;
