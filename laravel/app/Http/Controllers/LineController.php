5b6a6559da6efa1e3530c5f33864714e32c62308
iitenkida7 <i.takeshi2222@gmail.com>
2019-12-06 01:39:39 +0900

    ユーザ情報がDBにあるか確認

--- laravel/app/Http/Controllers/LineController.php
+++ laravel/app/Http/Controllers/LineController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Config;
 use App\Libs\reserveManager;
+use App\Libs\RegistUser;
+
 
 class LineController extends Controller
 {
@@ -15,6 +17,14 @@ class LineController extends Controller
         $requestBody = file_get_contents('php://input');
         Log::debug($requestBody);
         foreach (json_decode($requestBody, true)['events'] as $event){
+
+
+            if((new RegistUser)->isUser($event['source']['userId'])){
+                Log::debug("DB登録あり");
+            }else{
+                Log::debug("DBとうろくなし");
+            }
+
             // リクエスト検証
             $signature =  base64_encode(hash_hmac('sha256', $requestBody, Config::get('bike_share.line.channelSecret'), true));
             if($signature !== $requestHeaders['X-Line-Signature']){
--- laravel/app/Libs/RegistUser.php
+++ laravel/app/Libs/RegistUser.php
@@ -1,1 +1,19 @@
 <?php
+
+namespace App\Libs;
+
+use App\LineUser;
+
+class RegistUser
+{
+    function __construct()
+    {
+
+    }
+
+    public function isUser($lineId): bool
+    {
+        return LineUser::where('line_id', $lineId)->exists();
+    }
+
+}
