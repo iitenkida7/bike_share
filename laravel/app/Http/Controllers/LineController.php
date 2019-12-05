4ad3b01f6d2f3e7b3fa52b05cad051c07bbf57b8
iitenkida7 <i.takeshi2222@gmail.com>
2019-12-06 01:19:55 +0900

    複数のメッセージが１Webhookでやってくる可能性があるので繰り返す

--- laravel/app/Http/Controllers/LineController.php
+++ laravel/app/Http/Controllers/LineController.php
@@ -14,30 +14,28 @@ class LineController extends Controller
         $requestHeaders = getallheaders();
         $requestBody = file_get_contents('php://input');
         Log::debug($requestBody);
-        $event = json_decode($requestBody, true)['events'][0];
-  
-        // リクエスト検証
-        $signature =  base64_encode(hash_hmac('sha256', $requestBody, Config::get('bike_share.line.channelSecret'), true));
-        if($signature !== $requestHeaders['X-Line-Signature']){
-            header( "HTTP/1.1 404 Not Found" ) ;
-            exit;
-        }
-        // Lineからのリクエストにmessage が含まれるか
-        if(empty($event['message']['type'])){
-            header( "HTTP/1.1 415 Unsupported Media Type" ) ;
-            exit;
-        }
+        foreach (json_decode($requestBody, true)['events'] as $event){
+            // リクエスト検証
+            $signature =  base64_encode(hash_hmac('sha256', $requestBody, Config::get('bike_share.line.channelSecret'), true));
+            if($signature !== $requestHeaders['X-Line-Signature']){
+                header( "HTTP/1.1 404 Not Found" ) ;
+                exit;
+            }
+            // Lineからのリクエストにmessage が含まれるか
+            if(empty($event['message']['type'])){
+                header( "HTTP/1.1 415 Unsupported Media Type" ) ;
+                exit;
+            }
 
-        // メッセージタイプは位置情か
-        if ( $event['message']['type'] == 'location'){
-            (new reserveManager())->lineReceiver($event);
-        }
+            // メッセージタイプは位置情か
+            if ( $event['message']['type'] == 'location'){
+                (new reserveManager())->lineReceiver($event);
+            }
 
-        // メッセージタイプはtextか
-        if ( $event['message']['type'] == 'text'){
-            (new reserveManager())->lineMessageDispatcher($event);
-        }
-
-
+            // メッセージタイプはtextか
+            if ( $event['message']['type'] == 'text'){
+                (new reserveManager())->lineMessageDispatcher($event);
+            }
+         }
     }
 }
