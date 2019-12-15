17a736af01871c97b6578db1ac44fb61f43590f6
iitenkida7 <i.takeshi2222@gmail.com>
2019-12-15 22:29:59 +0900

    LINE友だち追加微修正

--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -5,8 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link href="https://unpkg.com/basscss@8.0.2/css/basscss.min.css" rel="stylesheet">
-
-        <script src="https://d.line-scdn.net/r/web/social-plugin/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
         <title>Chiyokuru Bot</title>
         <!-- Styles -->
         <style>
@@ -21,7 +19,8 @@
             <li>サービスは非公式です。予告なくサービス終了など行われる可能性があります。ご理解のうえ、ご利用ください。</li>
             <li>本サービスは、あなたの「ちよくる」アカウント(ID/Pass)を保持します。暗号化して保存しますが何かあっても責任は負いかねます。</li>
         </ul>
-         <div class="line-it-button" data-lang="ja" data-type="friend" data-lineid="@556inixh" style="display: none;"></div>
+         <div class="line-it-button" data-lang="ja" data-type="friend" data-lineid="@556inixh" data-count="true" data-home="true" style="display: none;"></div>
+         <script src="https://d.line-scdn.net/r/web/social-plugin/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
     </div>
     </body>
 </html>
