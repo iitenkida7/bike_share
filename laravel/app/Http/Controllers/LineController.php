9dcd9ab5d1c189921caa9a788d89e7a40fbd19e2
iitenkida7 <i.takeshi2222@gmail.com>
2019-12-15 12:46:38 +0900

    lineID登録済みか判定

--- laravel/app/Http/Controllers/LineController.php
+++ laravel/app/Http/Controllers/LineController.php
@@ -16,36 +16,40 @@ class LineController extends Controller
         $requestHeaders = getallheaders();
         $requestBody = file_get_contents('php://input');
         Log::debug($requestBody);
+        // リクエスト検証
+        $signature =  base64_encode(hash_hmac('sha256', $requestBody, Config::get('bike_share.line.channelSecret'), true));
+        if($signature !== $requestHeaders['X-Line-Signature']){
+            header( "HTTP/1.1 404 Not Found" ) ;
+            exit;
+        }
+
         foreach (json_decode($requestBody, true)['events'] as $event){
 
-
             if((new RegistUser)->isUser($event['source']['userId'])){
+            //if(true){
+
                 Log::debug("DB登録あり");
+                // Lineからのリクエストにmessage が含まれるか
+                if(empty($event['message']['type'])){
+                    header( "HTTP/1.1 415 Unsupported Media Type" ) ;
+                    exit;
+                }
+
+                // メッセージタイプは位置情か
+                if ( $event['message']['type'] == 'location'){
+                    (new reserveManager())->lineReceiver($event);
+                }
+
+                // メッセージタイプはtextか
+                if ( $event['message']['type'] == 'text'){
+                    (new reserveManager())->lineMessageDispatcher($event);
+                }
+
             }else{
-                Log::debug("DBとうろくなし");
+                (new RegistUser())->registAnnounce($event);
             }
 
-            // リクエスト検証
-            $signature =  base64_encode(hash_hmac('sha256', $requestBody, Config::get('bike_share.line.channelSecret'), true));
-            if($signature !== $requestHeaders['X-Line-Signature']){
-                header( "HTTP/1.1 404 Not Found" ) ;
-                exit;
-            }
-            // Lineからのリクエストにmessage が含まれるか
-            if(empty($event['message']['type'])){
-                header( "HTTP/1.1 415 Unsupported Media Type" ) ;
-                exit;
-            }
 
-            // メッセージタイプは位置情か
-            if ( $event['message']['type'] == 'location'){
-                (new reserveManager())->lineReceiver($event);
-            }
-
-            // メッセージタイプはtextか
-            if ( $event['message']['type'] == 'text'){
-                (new reserveManager())->lineMessageDispatcher($event);
-            }
          }
     }
 }
