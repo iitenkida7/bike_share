2d961c67633278baddaf382a7e601afef51a816a
iitenkida7 <i.takeshi2222@gmail.com>
2019-12-15 14:48:43 +0900

    トップページ

---
+++ laravel/resources/views/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link href="https://unpkg.com/basscss@8.0.2/css/basscss.min.css" rel="stylesheet">
+
+        <script src="https://d.line-scdn.net/r/web/social-plugin/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
+        <title>Chiyokuru Bot</title>
+        <!-- Styles -->
+        <style>
+        </style>
+    </head>
+    <body>
+    <div class="content">
+
+        <h1>Chiyokuru Bot</h1>
+        <ul>  
+            <li>ちよくる の自転車予約 を LINE上で行えるBotです。以下からLINEの友達追加をしてご利用ください。</li>
+            <li>サービスは非公式です。予告なくサービス終了など行われる可能性があります。ご理解のうえ、ご利用ください。</li>
+            <li>本サービスは、あなたの「ちよくる」アカウント(ID/Pass)を保持します。暗号化して保存しますが何かあっても責任は負いかねます。</li>
+        </ul>
+         <div class="line-it-button" data-lang="ja" data-type="friend" data-lineid="@556inixh" style="display: none;"></div>
+    </div>
+    </body>
+</html>
