e92dfab0d197caec488e8b13c3dd0733d41187c7
iitenkida7 <i.takeshi2222@gmail.com>
2019-12-15 13:26:57 +0900

    不要なenv,config 削除

--- laravel/config/bike_share.php
+++ laravel/config/bike_share.php
@@ -1,14 +1,8 @@
 <?php
 
 return [
-    'chiyokuru' => [
-        'memberId' => env('MEMBER_ID'),
-        'password' => env('PASSWORD'),
-    ],
-
     'line' => [
         'channelAccessToken' => env('LINE_CHANNEL_ACCESS_TOKEN'),
         'channelSecret'      => env('LINE_CHANNEL_SECRET'),
-        'userId'             => env('LINE_USER_ID'),
         ],
     ];
