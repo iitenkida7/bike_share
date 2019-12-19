754aa7f2e6bb0f835582c14a846051c4e297e459
iitenkida7 <i.takeshi2222@gmail.com>
2019-12-20 01:23:06 +0900

    Lineからの受信は、middleware を使って検証

--- laravel/app/Http/Middleware/CheckLine.php
+++ laravel/app/Http/Middleware/CheckLine.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Log;
 use Closure;
 
 class CheckLine
@@ -14,7 +16,16 @@ class CheckLine
      * @return mixed
      */
     public function handle($request, Closure $next)
-    {
+    {    
+        $signature =  base64_encode(hash_hmac('sha256', $request->getContent(), Config::get('bike_share.line.channelSecret'), true));
+        if($signature !== $request->header('X-Line-Signature')){
+            Log::debug($request->getContent());
+            abort(403, 'Unauthorized action.');
+        }
+        if(empty(json_decode($request->getContent(), true)['events'][0]['message']['type'])){
+            Log::debug($request->getContent());
+            abort(403, 'Unauthorized action.');
+         }
         return $next($request);
     }
 }
