2820fa8f5e0f7299b20335f81507ffdc7f7212e4
iitenkida7 <i.takeshi2222@gmail.com>
2019-12-20 00:51:57 +0900

    laravel の標準メソッド利用

--- laravel/app/Http/Controllers/LineController.php
+++ laravel/app/Http/Controllers/LineController.php
@@ -11,20 +11,18 @@ use App\Libs\RegistUser;
 
 class LineController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $requestHeaders = getallheaders();
-        $requestBody = file_get_contents('php://input');
-        Log::debug($requestBody);
+        Log::debug($request->getContent());
         // リクエスト検証
-        $signature =  base64_encode(hash_hmac('sha256', $requestBody, Config::get('bike_share.line.channelSecret'), true));
+        $signature =  base64_encode(hash_hmac('sha256', $request->getContent(), Config::get('bike_share.line.channelSecret'), true));
         if($signature !== $requestHeaders['X-Line-Signature']){
             header( "HTTP/1.1 404 Not Found" ) ;
             exit;
         }
 
-        foreach (json_decode($requestBody, true)['events'] as $event){
-
+        foreach (json_decode($request->getContent(), true)['events'] as $event){
             if((new RegistUser)->isUser($event['source']['userId'])){
                 Log::debug("DB登録あり");
                 // Lineからのリクエストにmessage が含まれるか
